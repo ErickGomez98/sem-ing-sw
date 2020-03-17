a9ad6db591371ddfed3a0754ab2c3f88e2a18663
Erick Gómez <erick0gomez98@gmail.com>
2020-03-17 13:04:56 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+//@ts-ignore
+import { AnimatedSwitch } from "react-router-transition";
+import "./App.css";
+import Results from "./pages/results";
+import Statistics from "./pages/statistics";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className="switch-wrapper"
+      >
+        <Route exact path="/results" component={Results} />
+        <Route exact path="/statistics" component={Statistics} />
+        <Route exact path="/" component={Home} />
+        <Redirect to="/" />
+      </AnimatedSwitch>
+    </Router>
   );
 }
 
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Layout() {
+  return (
+    <div>
+      <div>layout left img</div>
+      <div>layour right</div>
+    </div>
+  );
+}
+
+export default Layout;
