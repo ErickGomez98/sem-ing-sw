6d3674d1236ec94d1786d3a72a268d8fc58f6b46
Erick Gómez <erick0gomez98@gmail.com>
2020-04-11 16:54:08 -0500

    se implementó la conexión con la bd en mongodb atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { CatsModule } from './cats/cats.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@node-routes-fzx97.mongodb.net/test?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
