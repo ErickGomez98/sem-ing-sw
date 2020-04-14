2adaa8753399718c97e9cc9f1d8dc3d0aee66bf1
Erick Gómez <erick0gomez98@gmail.com>
2020-04-14 15:57:16 -0500

    Se modifico la variable de entorno que guarda la conexión a la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-
 @Module({
   imports: [
     CatsModule,
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@node-routes-fzx97.mongodb.net/test?retryWrites=true&w=majority`,
-    ),
+    MongooseModule.forRoot(`${process.env.DB_CONNECTION}`),
   ],
   controllers: [AppController],
   providers: [AppService],
